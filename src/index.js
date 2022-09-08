7eb509cf18b5cecab57600e16d81da8ac02e7ec1
AnderG <agibarguen@gmail.com>
2022-09-08 19:49:16 +0200

    Team Page rendering

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import { pageLoad } from "./homePageModule.js";
 import { Team } from "./teamPageModule.js";
 import ClaudioRooster from "./claudio.png";
+import FamilyGuyRooster from "./familyGuyRooster.png";
+import Jerry from "./jerry-smith.png"
 import './style.css';
 
 //render a title, img and a p for the home page
 const homePageEl = document.getElementById('home')
+
 const container = document.getElementById('content')
+container.style.cssText = `
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    gap: 3em;
+    justify-content: center;
+    align-items: center;
+`
+
 pageLoad(container,ClaudioRooster)
 
 homePageEl.addEventListener('click', ()=>{
     container.innerHTML = ""
+    container.style.cssText = `
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    gap: 3em;
+    justify-content: center;
+    align-items: center;
+`
     pageLoad(container,ClaudioRooster)
 })
 
@@ -17,8 +37,17 @@ homePageEl.addEventListener('click', ()=>{
 const teamPageEl = document.getElementById('team')
 teamPageEl.addEventListener('click', ()=>{
     container.innerHTML = ""
-    const memberOne = new Team('NUmber One', 'Very Beautiful')
-    memberOne.render(container)
+    container.style.cssText = `
+        display: grid;
+        grid-template-columns:repeat(2,1fr);
+        gap: 3em 1em;
+`
+
+    const memberOne = new Team('Number One', FamilyGuyRooster, 'Very Beautiful')
+    memberOne.render(container, FamilyGuyRooster)
+
+    const memberTwo = new Team('Number Two',Jerry, 'Always running')
+    memberTwo.render(container, Jerry)
 })
 
 
--- src/teamPageModule.js
+++ src/teamPageModule.js
@@ -1,28 +1,28 @@
-/*export function teamRendering(element){
-    let teamTitle = document.createElement('h1')
-    teamTitle.textContent = 'Team member number 1'
-
-    element.appendChild(teamTitle)
-
-    let memberDescription = document.createElement('p')
-    memberDescription.textContent = 'Very beautiful'
-    element.appendChild(memberDescription)
-}*/
 
 export class Team{
-    constructor(member,description){
+    constructor(member,icon,description){
         this.member = member
-        //this.icon = icon
+        this.icon = icon
         this.description = description
     }
     render = (element) =>{
+        let memberContainer = document.createElement('div')
+        memberContainer.setAttribute('class', 'memberContainer')
+
         let teamTitle = document.createElement('h1')
         teamTitle.textContent = this.member
     
-        element.appendChild(teamTitle)
+        memberContainer.appendChild(teamTitle)
+
+        const memberIcon = new Image()
+        memberIcon.src = this.icon
+
+        memberContainer.appendChild(memberIcon)
     
         let memberDescription = document.createElement('p')
         memberDescription.textContent = this.description
-        element.appendChild(memberDescription)
+        memberContainer.appendChild(memberDescription)
+
+        element.appendChild(memberContainer)
     }
 }
